3ea205f20001ba37202e4df2af28c9eef9e41d08
PlagueManRodi <114426282+PlagueManRodi@users.noreply.github.com>
2025-02-13 01:29:59 -0500

    Update noctowl.asm
    buff bst to better match level attained

--- data/pokemon/base_stats/noctowl.asm
+++ data/pokemon/base_stats/noctowl.asm
@@ -2,7 +2,7 @@ if DEF(FAITHFUL)
 	db 100,  50,  50,  70,  86,  96 ; 452 BST
 	;   hp  atk  def  spe  sat  sdf
 else
-	db 100,  50,  50,  70,  96,  96 ; 462 BST
+	db 110,  50,  50,  70,  96, 106 ; 482 BST
 	;   hp  atk  def  spe  sat  sdf
 endc
 
