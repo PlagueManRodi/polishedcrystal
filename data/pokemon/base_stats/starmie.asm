ff8ae30a8e711161590c0343fed2977f771ed11a
PlagueManRodi <114426282+PlagueManRodi@users.noreply.github.com>
2026-02-25 18:58:24 -0500

    Update starmie.asm

--- data/pokemon/base_stats/starmie.asm
+++ data/pokemon/base_stats/starmie.asm
@@ -7,7 +7,7 @@
 	db STARDUST, STAR_PIECE ; held items
 	dn GENDER_UNKNOWN, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
 
-	abilities_for STARMIE, ILLUMINATE, NATURAL_CURE, ANALYTIC
+	abilities_for STARMIE, ANALYTIC, NATURAL_CURE, REGENERATOR
 	db GROWTH_SLOW ; growth rate
 	dn EGG_WATER_3, EGG_WATER_3 ; egg groups
 
