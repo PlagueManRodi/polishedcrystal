32e8cdb0fea476cb8dde100075a57c4c8f25f504
PlagueManRodi <114426282+PlagueManRodi@users.noreply.github.com>
2026-02-26 00:21:47 -0500

    Update skarmory.asm

--- data/pokemon/base_stats/skarmory.asm
+++ data/pokemon/base_stats/skarmory.asm
@@ -2,7 +2,7 @@ if DEF(FAITHFUL)
 	db  65,  80, 140,  70,  40,  70 ; 465 BST
 	;   hp  atk  def  spe  sat  sdf
 else
-	db  85,  80, 140,  70,  40,  70 ; 485 BST
+	db  85,  95, 140,  70,  40,  70 ; 500 BST
 	;   hp  atk  def  spe  sat  sdf
 endc
 
