32fb320c0d49de0dbc0b9adb5a8b9cb51b144799
PlagueManRodi <114426282+PlagueManRodi@users.noreply.github.com>
2026-02-26 00:35:37 -0500

    Update rapidash_galarian.asm

--- data/pokemon/base_stats/rapidash_galarian.asm
+++ data/pokemon/base_stats/rapidash_galarian.asm
@@ -1,5 +1,10 @@
+if DEF(FAITHFUL)
 	db  65, 100,  70, 105,  80,  80 ; 500 BST
 	;   hp  atk  def  spe  sat  sdf
+else
+	db  75, 100,  70, 110,  80,  90 ; 525 BST
+	;   hp  atk  def  spe  sat  sdf
+endc
 
 	db PSYCHIC, FAIRY ; type
 	db 60 ; catch rate
